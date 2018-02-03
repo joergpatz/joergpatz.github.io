70a6cf4fc49a79640e68fedb02986d9354fb6405
Joerg Patzer <joerg.patzer@me.com>
2018-02-03 18:57:47 +0100

    add static html support

--- layout/colors.js
+++ layout/colors.js
@@ -1,9 +1,16 @@
+import SunCalc from "suncalc";
+
 export const black = '#000000';
-
 export const white = '#ffffff';
-
 export const grey = '#9b9b9b';
 export const gray = grey;
 
-export const background = white;
-export const foreground = black;
+const times = SunCalc.getTimes(new Date(), 51.5, -0.1);
+const date = new Date();
+const gmtDate = new Date(date.valueOf() + date.getTimezoneOffset() * 60 * 1000);
+
+//From Dusk Till Dawn
+const isDarkTheme = gmtDate > times.dusk && gmtDate < times.dawn;
+
+export const background = isDarkTheme ? black : white;
+export const foreground = isDarkTheme ? white : black;
